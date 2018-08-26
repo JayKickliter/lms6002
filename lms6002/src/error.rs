7ef234e6a67231cb46866f9c8f85de773a46dcf2
Jay Kickliter <jay@kickliter.com>
2018-08-26 16:37:36 -0700

    lib: add Range error variant and make Error comparable.

--- lms6002/src/error.rs
+++ lms6002/src/error.rs
@@ -2,9 +2,11 @@
 pub type Result<T> = ::std::result::Result<T, Error>;
 
 /// Error type returned by `LMS6002` methods.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     /// An error originating from the underlying (user provided)
     /// interface.
     IO,
+    /// A parameter is not in range.
+    Range,
 }
