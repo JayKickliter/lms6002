6dec692876bad50232804f26a18a68679098d143
Jay Kickliter <jay@kickliter.com>
2018-08-31 14:00:37 -0700

    lib/err: derive PartialEq on Error for testing.

--- lms6002/src/error.rs
+++ lms6002/src/error.rs
@@ -2,7 +2,7 @@
 pub type Result<T> = ::std::result::Result<T, Error>;
 
 /// Error type returned by `LMS6002` methods.
-#[derive(Fail, Debug)]
+#[derive(Fail, Debug, PartialEq)]
 pub enum Error {
     /// An error originating from the underlying (user provided)
     /// interface.
@@ -17,7 +17,7 @@ pub enum Error {
 }
 
 /// An error indicating failure to find suitable PLL parameters.
-#[derive(Fail, Debug)]
+#[derive(Fail, Debug, PartialEq)]
 pub enum PllError {
     #[fail(display = "Failure to find a suitable VCO")]
     VcoSel,
